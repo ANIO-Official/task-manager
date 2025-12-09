89e5602e9ab8427a8dbf458abe4828c626131193
Amanda Ogletree <amandaogletree19@gmail.com>
2025-12-09 15:34:47 -0500

    Created logic for App. Added TaskList to JSX. Tested that list renders in browser.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,69 @@
 import { useState } from 'react'
 
 import './App.css'
+import TaskList from './components/TaskList/TaskList'
+import type { Task, TaskStatus } from './types'
+import TaskItem from './components/TaskItem/TaskItem'
 
 function App() {
-  
+
+  //Task Data
+  let tasks: Task[] = [
+      {
+        id: '1',
+        title: 'Task 1',
+        description: 'Task 1 description.',
+        status: 'pending',
+        priority: 'low',
+        dueDate: '10/11/2025'
+      },
+
+      {
+        id: '2',
+        title: 'Task 2',
+        description: 'Task 2 description.',
+        status: 'in-progress',
+        priority: 'medium',
+        dueDate: '11/1/2025'
+      },
+
+      {
+        id: '3',
+        title: 'Task 3',
+        description: 'Task 3 description',
+        status: 'completed',
+        priority:'high',
+        dueDate: '01/06/2025'
+      }
+  ]
+  const [tasksData, setTaskData] = useState(tasks)
+
+  const handleStatusChange = (taskId:string, newStatus:TaskStatus) =>{
+    /*
+      Find the task item by ID from the object data array 'tasksData'.
+      Set the status of this specific item.
+    */
+    const taskItem = tasksData.filter((task) =>
+      task.id === taskId
+    )[0]
+    taskItem.status = newStatus
+  }
+
+  const handleTaskDeletion = (taskId: string) =>{
+    const taskItem = tasksData.filter((task) =>
+      task.id === taskId
+    )[0]
+    const indexOfTask = tasks.indexOf(taskItem, 1)
+    return tasksData.splice(indexOfTask)
+  }
 
   return (
     <>
-      
+      <TaskList
+      tasks={tasksData}
+      onStatusChange={handleStatusChange}
+      onDelete={handleTaskDeletion}
+      />
     </>
   )
 }
--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -1,11 +1,39 @@
-import type { TaskItemProps } from "../../types"
+import { useState } from "react"
+import type { TaskItemProps, TaskStatus } from "../../types"
 
 
+/*
+    Props:
+    1. Task -> Task typed Object with data
+    2. onStatusChange - > Function to change status. Takes a task id & newStatus value
+    3. onDelete -> Deletes task. Takes a Task ID
+
+*/
 export default function TaskItem({ task, onStatusChange, onDelete }: TaskItemProps) {
     
+    const [selectedValue, setSelectedValue] = useState<string>(task.status)
+    
     const handleStatusChange = (event: React.ChangeEvent<HTMLSelectElement>) =>{
-        /* Send this value as the new value for the onStatusChange function to use*/
-        const newStatus = event.target.value
+
+        //Set New Select Value on Change
+        setSelectedValue(event.target.value)
+
+        /*
+            Change the TaskStatus type based on the value of the select.
+            Cannot explicitly state newStatus = event.target.value because
+            the select value returns a string. And though the TaskStatus type
+            uses strings, they are still not technically of the same type. (I know, I tried.)
+        */
+        let newStatus:TaskStatus
+        if (event.target.value === 'pending'){
+            newStatus = 'pending'
+        }
+        else if (event.target.value === 'in-progress'){
+            newStatus = 'in-progress'
+        }
+        else{newStatus = 'completed'}
+
+        //set newStatus as the variable for the onStatusChange function prop to use.
         onStatusChange (task.id, newStatus)
     }
 
@@ -13,11 +41,11 @@ export default function TaskItem({ task, onStatusChange, onDelete }: TaskItemPro
         onDelete(task.id)
     }
     
-    function createTask() {
+    const createTask = (
             <div>
                 <div>
                     <h2>{task.title}</h2>
-                    <select onChange={handleStatusChange}>
+                    <select onChange={handleStatusChange} value={selectedValue}>
                         {/* Set the values to the string values of the type */}
                         <option value="pending">Pending</option>
                         <option value="in-progress">In Progress</option>
@@ -29,12 +57,11 @@ export default function TaskItem({ task, onStatusChange, onDelete }: TaskItemPro
                 <p>{task.description}</p>
                 <p>{task.priority}</p>
             </div>
-    }
-
+    )
     //Create Task Item
     return (
         <>
-          {createTask()}
+          {createTask}
         </>
     )
 }
